3a388b58edf99fd1297515609c2abe5a6dfaa4d2
Prakash-78 <middiliomprakashreddy@gmail.com>
2026-02-18 20:44:12 +0530

    Today learned Diff staged/unstaged changes

--- Run.py
+++ Run.py
@@ -1,1 +1,8 @@
 print(f"{'-'*10} Welocme to learning GIT&GITHUB{'-'*10}")
+print("Today -> 17/02/26 Learning basic git commands and commiting them to github remote repository")
+
+# Today learning git branching  18/02/26
+
+
+
+print("Git is a version control system that will help you to track changes in the files")
